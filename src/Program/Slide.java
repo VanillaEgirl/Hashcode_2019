66d1bd60870df5abe9227a429c3831cb5fc1fd41
StefanTieber <38786543+StefanTieber@users.noreply.github.com>
2019-02-28 19:01:03 +0100

    slide

--- src/Program/Slide.java
+++ src/Program/Slide.java
@@ -1,11 +1,19 @@
 package Program;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Slide {
     List<Photo> photos;
 
     List<String> getTags() {
-        return null;
+        List<String> tags = new ArrayList<>();
+
+        for (Photo photo : photos) {
+            tags.addAll(photo.tags);
+        }
+
+        return tags.stream().distinct().collect(Collectors.toList());
     }
 }
