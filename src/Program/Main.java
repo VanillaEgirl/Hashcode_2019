36e5898efecdc51eff9597f34dafd96f54d6ab8e
StefanTieber <38786543+StefanTieber@users.noreply.github.com>
2019-02-28 13:49:23 +0100

    test

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> lines = FileReader.readLines();
-        System.out.println();
+        System.out.println("Hallo du Schneckli!");
         FileWriter.writeLines(lines);
     }
 }
