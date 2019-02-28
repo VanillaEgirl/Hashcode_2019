aa0e3c9b76b02760301c6da7535b1c02577b1fa0
StefanTieber <38786543+StefanTieber@users.noreply.github.com>
2019-02-28 19:12:47 +0100

    interesting

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -21,4 +21,34 @@ public class Main {
         slides.add(slide2);
         FileWriter.writeSlideshow(slides);
     }
+
+    public static int howInteresting(Slide slide1, Slide slide2) {
+        int onlySlide1 = 0;
+        int onlySlide2 = 0;
+        int both = 0;
+        int minimum;
+
+        List<String> tags1 = slide1.getTags();
+        List<String> tags2 = slide2.getTags();
+
+        for (String tag : tags1) {
+            if (tags2.contains(tag)) {
+                both++;
+            } else {
+                onlySlide1++;
+            }
+        }
+
+        for (String tag : tags2) {
+            if (!tags1.contains(tag)) {
+                onlySlide1++;
+            }
+        }
+
+        minimum = both;
+        if (onlySlide1 < minimum) minimum = onlySlide1;
+        if (onlySlide2 < minimum) minimum = onlySlide2;
+
+        return minimum;
+    }
 }
