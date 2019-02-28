f1f40dfba6bd2ddf59adabb99b787bf18d7b29d0
StefanTieber <38786543+StefanTieber@users.noreply.github.com>
2019-02-28 19:17:52 +0100

    s

--- src/Program/Slide.java
+++ src/Program/Slide.java
@@ -16,4 +16,34 @@ public class Slide {
 
         return tags.stream().distinct().collect(Collectors.toList());
     }
+
+    public int howInteresting(Slide slide) {
+        int onlySlide1 = 0;
+        int onlySlide2 = 0;
+        int both = 0;
+        int minimum;
+
+        List<String> tags1 = getTags();
+        List<String> tags2 = slide.getTags();
+
+        for (String tag : tags1) {
+            if (tags2.contains(tag)) {
+                both++;
+            } else {
+                onlySlide1++;
+            }
+        }
+
+        for (String tag : tags2) {
+            if (!tags1.contains(tag)) {
+                onlySlide1++;
+            }
+        }
+
+        minimum = both;
+        if (onlySlide1 < minimum) minimum = onlySlide1;
+        if (onlySlide2 < minimum) minimum = onlySlide2;
+
+        return minimum;
+    }
 }
